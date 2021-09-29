38ce5e48e14da319c4916dc9afe4d418da9ef391
{psw9058) <s05150732w@gmail.com>
2021-09-29 13:10:34 +0900

    (7-10)

--- 7-10/app.js
+++ 7-10/app.js
@@ -1,10 +1,27 @@
 const input = require('fs').readFileSync('./input.txt').toString().trim().split('\n');
 
-let group = 0;
+const caseCount = Number(input[0]);
+let countGroupWord = 0;
 
-for (let i = 1; i <= input[0]; i++) {
-  for(let j = 0; j < input[i].length; j++) {
-    console.log(input[i][j]);
-    
+for (let i = 1; i <= caseCount; i++) {
+  const word = input[i];
+  const letter = [];
+  let isGroupWord = true;
+
+  for (let j = 0; j < word.length; j++) {
+    if (letter.indexOf(word[j]) === -1) {
+      letter.push(word[j]);
+    } else {
+      if (letter.indexOf(word[j]) !== letter.length - 1) {
+        isGroupWord = false;
+        break;
+      }
+    }
+  }
+
+  if (isGroupWord) {
+    countGroupWord += 1;
   }
 }
+
+console.log(countGroupWord);
