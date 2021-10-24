8edb149612784c2b17f4b8e08173c09e5e89be89
{psw9058) <s05150732w@gmail.com>
2021-10-24 14:52:11 +0900

    8-9 python

--- 8-9/app.js
+++ 8-9/app.js
@@ -1,9 +1,14 @@
-let input = require('fs').readFileSync('./input.txt').toString().split('\n');
+T = int(input()) 
 
-let caseNum = Number(input.shift());
+for i in range(T): 
+  x, y = map(int, input().split()) 
+  #출발 및 도착 지점 
+  d = y - x #거리 
+  n = 0 while True: 
+    if d <= n*(n+1):
+      break n += 1 
+      
+  #총 이동 거리가 n의 제곱보다 작거나 같을 때 
+    if d <= n**2: print(n*2-1) 
 
-for(let i = 0; i < caseNum; i++) {
-  let arr = input[i].split(' ');
-  let range = arr[1] - arr[0] - 1;
-  console.log(range);
-}
+  #총 이동 거리가 n의 제곱보다 클 때 else: print(n*2)
