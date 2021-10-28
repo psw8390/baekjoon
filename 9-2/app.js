9b31ff49e395a83ca681ea5e32b6ca4e566a0045
{psw9058) <s05150732w@gmail.com>
2021-10-28 20:39:45 +0900

    9-2 submit

--- 9-2/app.js
+++ 9-2/app.js
@@ -1,37 +1,33 @@
 const fs = require('fs');
-const input = fs.readFileSync('./input.txt').toString().split('\n');
-
-// 소수 판별 함수
-function isPrime(n) {
-  if (n === 2 || n === 3) {
-    return true;
-  }
-
-  if (n <= 1 || n % 2 === 0) {
-    return false;
-  }
-
-  for (let i = 3; i <= Math.sqrt(n); i += 2) {
-    if (n % i === 0) {
-      return false;
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+ 
+const primeNumArr = [];
+let primeNumSum = 0;
+ 
+function primeNumber(n) {
+  if (n < 2) {
+    return;
+}
+ 
+for (let i = 2; i < n; i++) {
+  if (n % i === 0) {
+    return;
     }
   }
-
-  return true;
+  primeNumArr.push(n);
+  primeNumSum += n;
 }
-
-let count = [];
-let sum = 0;
-for (let i = 60; i <= input[1] && i >= input[0]; i++) {
-  if (isPrime(i)) {
-    count.push(i);
-    sum = sum + i;
-  }
+ 
+const begin = parseInt(input.shift());
+const end = parseInt(input.shift());
+ 
+for (let i = begin; i <= end; i++) {
+  primeNumber(i);
 }
-
-if(sum === 0){
+ 
+if (!primeNumArr.length) {
   console.log(-1);
-}else {
-  console.log(sum);
-  console.log(count[0]);
+} else {
+  console.log(primeNumSum);
+  console.log(Math.min.apply(null, primeNumArr));
 }
