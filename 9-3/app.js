c05c1fe6fe061b88cc52ff17145a56f2c2161ab4
{psw9058) <s05150732w@gmail.com>
2021-10-29 16:43:44 +0900

    9-3 submit

--- 9-3/app.js
+++ 9-3/app.js
@@ -1,35 +1,13 @@
-const fs = require('fs');
-let input = fs.readFileSync('./input.txt').toString().trim();
+const fs = require("fs");
+let input = +(process.platform === "linux"
+  ? fs.readFileSync("/dev/stdin").toString().trim()
+  : `5`);
 
-// 소수 판별 함수
-function isPrime(n) {
-  if (n === 2 || n === 3) {
-    return true;
-  }
-
-  if (n <= 1 || n % 2 === 0) {
-    return false;
-  }
-
-  for (let i = 3; i <= Math.sqrt(n); i += 2) {
-    if (n % i === 0) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-let sosu = [];
-for (let i = 0; i <= input; i++) {
-  if (isPrime(i)) {
-    sosu.push(i);
+let result = [];
+for (let i = 2; i <= input; i++) {
+  while (input % i === 0) {
+    input = input / i;
+    result.push(i);
   }
 }
-
-for(let i = 0; i < sosu.length; i++) {
-  while(input % sosu[i] === 0) {
-    input = input / sosu[i];
-    console.log(sosu[i]);
-  }
-}
+console.log(result.join("\n"));
