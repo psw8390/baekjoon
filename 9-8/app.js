62c5b55d5562a651eb623d7283958169fcf1857d
{psw9058) <s05150732w@gmail.com>
2021-11-09 16:22:28 +0900

    9-8

--- 9-8/app.js
+++ 9-8/app.js
@@ -1,16 +1,33 @@
 var fs = require('fs');
-var input = fs.readFileSync('./input.txt').toString().split(' ');
- 
-var [x, y, w, h] = input.map(el => parseInt(el));
- 
-//w, h 는 x, y보다 크기 떄문에 항상 양수
-const xDiff = w-x
-const yDiff = h-y
- 
-// 네개의 변수 중 최소값이 문제에서 하는 최소 거리이다.
-const arr = [xDiff, yDiff, x, y]
-// js의 기본 sort는 ascii 문자를 기준으로 정렬하기 때문에 반드시 비교함수를 넣어줘야한다.
-arr.sort((a, b) => {
-  return a - b
-});
-console.log(arr[0])
+var input = fs.readFileSync('./input.txt').toString().split('\n');
+
+let x = [];
+let y = [];
+
+for (let i = 0; i <input.length; i++) {
+  input[i].split(' ');
+  x.push(input[i][0]);
+  y.push(input[i][2]);
+}
+
+let resultX = [];
+let resultY = [];
+
+
+if(x[0] === x[1]) {
+  resultX.push(x[2]);
+} else if(x[0] === x[2]) {
+  resultX.push(x[1]);
+} else if(x[1] === x[2]) {
+  resultX.push(x[0]);
+}
+
+if(y[0] === y[1]) {
+  resultY.push(y[2]);
+} else if(y[0] === y[2]) {
+  resultY.push(y[1]);
+} else if(y[1] === y[2]) {
+  resultY.push(y[0]);
+}
+
+console.log(`${resultX[0]} ${resultY[0]}`);
