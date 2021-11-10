10623dbb142f53610a7300bdf410b5568b686a3f
{psw9058) <s05150732w@gmail.com>
2021-11-10 15:26:55 +0900

    9-9 submit

--- 9-9/app.js
+++ 9-9/app.js
@@ -1,15 +1,23 @@
-let fs = require('fs');
-let input = fs.readFileSync('./input.txt').toString().split("\n");
+const fs = require('fs');
+const input = fs.readFileSync('./input.txt').toString().trim().split('\n');
 
-for (let i = 0; i < input.length; i++) {
-  let input2 = input[i].split(' ');
-  if(input[i][0] === '0') {
-    return;
+
+for (let sides of input) {
+  const strToInt = sides.split(' ').map(num => Math.pow(parseInt(num), 2));
+  const firstSidePow = strToInt.shift();
+  const secondSidePow = strToInt.shift();
+  const thirdSidePow = strToInt.shift();
+  if (firstSidePow === 0 && secondSidePow === 0 && thirdSidePow === 0) {
+    break;
+  }
+
+  if (
+    firstSidePow + secondSidePow === thirdSidePow ||
+    firstSidePow + thirdSidePow === secondSidePow ||
+    secondSidePow + thirdSidePow === firstSidePow
+  ) {
+    console.log('right');
   } else {
-    if (input2[0]*input2[0] + input2[1] * input2[1] === input2[2]*input2[2]) {
-      console.log('right');
-    } else {
-      console.log('wrong');
-    }
+    console.log('wrong');
   }
 }
