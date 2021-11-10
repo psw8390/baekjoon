3c22d0cd9ed316f6e2fa7e4e92ffff1e80e1d42a
{psw9058) <s05150732w@gmail.com>
2021-11-10 15:11:16 +0900

    9-9 first submit

--- 9-9/app.js
+++ 9-9/app.js
@@ -1,17 +1,15 @@
 let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split("\n");
-let arrayX = [];
-let arrayY = [];
-let x;
-let y;
-for(let i = 0; i < 3; i++){
-    arrayX.push(Number(input[i].split(" ")[0]));
-    arrayY.push(Number(input[i].split(" ")[1]));
+let input = fs.readFileSync('./input.txt').toString().split("\n");
+
+for (let i = 0; i < input.length; i++) {
+  let input2 = input[i].split(' ');
+  if(input[i][0] === '0') {
+    return;
+  } else {
+    if (input2[0]*input2[0] + input2[1] * input2[1] === input2[2]*input2[2]) {
+      console.log('right');
+    } else {
+      console.log('wrong');
+    }
+  }
 }
-arrayX = arrayX.sort();
-arrayy = arrayY.sort();
-
-x = arrayX[1] === arrayX[0] ? arrayX[2] : arrayX[0];
-y = arrayY[1] === arrayY[0] ? arrayY[2] : arrayY[0];
-
-console.log(`${x} ${y}`);
