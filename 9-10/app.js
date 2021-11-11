2651aab2dd4d7def1e843f01aae0f24c8b1ba2c2
{psw9058) <s05150732w@gmail.com>
2021-11-11 11:45:28 +0900

    9-10 submit

--- 9-10/app.js
+++ 9-10/app.js
@@ -1,6 +1,15 @@
-const fs = require('fs');
-const input = fs.readFileSync('./input.txt').toString().trim()
+// const fs = require('fs');
+// const input = parseInt(fs.readFileSync('dev/stdin').toString().trim());
 
-console.log(input);
+const input = 1;
 
-
+function Euclid(radius) {
+return Math.pow(radius, 2) * Math.PI;
+}
+ 
+function taxi(radius) {
+return Math.pow(radius, 2) * 2;
+}
+ 
+console.log(Euclid(input).toFixed(6));
+console.log(taxi(input).toFixed(6));
