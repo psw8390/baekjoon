68639e76a77498b0cc777a1e6f16648b577e3574
{psw9058) <s05150732w@gmail.com>
2021-11-25 14:40:15 +0900

    11-1 create

--- 10-4/app.js
+++ 10-4/app.js
@@ -1,2 +1,20 @@
 let fs = require('fs');
 let input = fs.readFileSync("./input.txt").toString();
+
+let N = Number(input); 
+let count = 0;
+let answer = [];
+
+function Hanoi(num, from, other, to){
+    if(num === 0) {
+        return;
+    }else{
+        Hanoi(num - 1 , from, to, other);
+        answer.push([from, to]);
+        count++;
+        Hanoi(num - 1, other, from, to);
+    }
+}
+Hanoi(N, '1','2','3');
+console.log(count);
+console.log(answer.map((element) => element.join(" ")).join("\n"));
