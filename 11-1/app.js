43925bd1510e94a7607339456f9b97eb708d98c7
{psw9058) <s05150732w@gmail.com>
2021-11-26 14:14:00 +0900

    11-1

--- 11-1/app.js
+++ 11-1/app.js
@@ -1,2 +1,33 @@
-let fs = require('fs');
-let input = fs.readFileSync("./input.txt").toString();
+// Blackjack
+ 
+// For submit
+ 
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+ 
+// For local test
+const input = ['5 21', '5 6 7 8 9'];
+const NM = input
+.shift()
+.split(' ')
+.map(num => parseInt(num));
+const N = NM.shift();
+const M = NM.shift();
+const cardArr = input
+.shift()
+.split(' ')
+.map(num => parseInt(num));
+let max = 0;
+ 
+for (let i = 0; i < N - 2; i++) {
+for (let j = i + 1; j < N - 1; j++) {
+for (let k = j + 1; k < N; k++) {
+let sum = cardArr[i] + cardArr[j] + cardArr[k];
+if (sum > max && sum <= M) {
+max = sum;
+}
+}
+}
+}
+ 
+console.log(max);
