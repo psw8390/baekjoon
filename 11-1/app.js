02ae6d224e870ed022139e4e8faf42f2f50399e5
{psw9058) <s05150732w@gmail.com>
2021-11-28 16:21:05 +0900

    11-1 submit2

--- 11-1/app.js
+++ 11-1/app.js
@@ -4,26 +4,26 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const input = ['5 21', '5 6 7 8 9'];
 const NM = input
-.shift()
-.split(' ')
-.map(num => parseInt(num));
+  .shift()
+  .split(' ')
+  .map(num => parseInt(num));
 const N = NM.shift();
 const M = NM.shift();
 const cardArr = input
-.shift()
-.split(' ')
-.map(num => parseInt(num));
+  .shift()
+  .split(' ')
+  .map(num => parseInt(num));
 let max = 0;
- 
+
 for (let i = 0; i < N - 2; i++) {
-for (let j = i + 1; j < N - 1; j++) {
-for (let k = j + 1; k < N; k++) {
-let sum = cardArr[i] + cardArr[j] + cardArr[k];
-if (sum > max && sum <= M) {
-max = sum;
+  for (let j = i + 1; j < N - 1; j++) {
+    for (let k = j + 1; k < N; k++) {
+      let sum = cardArr[i] + cardArr[j] + cardArr[k];
+      if (sum > max && sum <= M) {
+      max = sum;
+      }
+    }
+  }
 }
-}
-}
-}
- 
+
 console.log(max);
