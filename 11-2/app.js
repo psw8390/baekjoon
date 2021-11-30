a8afbf27b5ee37f1e68d33ca0131575ff9c5238c
{psw9058) <s05150732w@gmail.com>
2021-11-30 13:40:20 +0900

    11-2 submit

--- 11-2/app.js
+++ 11-2/app.js
@@ -1,3 +1,22 @@
 const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const input = parseInt(fs.readFileSync('/dev/stdin').toString().trim());
 
+const input = 216;
+const constructorArr = [];
+
+function d(n) {
+  const N = n.toString().split('');
+  return n + N.reduce((acc, num) => (acc += parseInt(num)), 0);
+}
+
+for (let i = 1; i <= input; i++) {
+  if (d(i) === input) {
+    constructorArr.push(i);
+  }
+}
+
+if (constructorArr.length) {
+  console.log(Math.min.apply(null, constructorArr));
+} else {
+  console.log(0);
+}cd
