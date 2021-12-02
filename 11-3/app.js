1ac39024df850b661c2117b9f4a1d96078fc3d06
{psw9058) <s05150732w@gmail.com>
2021-12-02 11:29:18 +0900

    11-3 submit

--- 11-3/app.js
+++ 11-3/app.js
@@ -1,3 +1,23 @@
 const fs = require('fs');
-const input = parseInt(fs.readFileSync('/dev/stdin').toString().trim());
+const input = fs.readFileSync('./input.txt').toString().trim().split('\n');
 
+const T = input.shift();
+const kgCmTable = input.map(kgCm => kgCm.split(' ').map(num => parseInt(num)));
+const rank = [];
+
+for (let i = 0; i < T; i++) {
+  let counter = 0;
+  for (let j = 0; j < T; j++) {
+    if (i !== j) {
+      if (
+        kgCmTable[i][0] < kgCmTable[j][0] &&
+        kgCmTable[i][1] < kgCmTable[j][1]
+      ) {
+        counter++;
+      }
+    }
+  }
+  rank.push(counter + 1);
+}
+
+  console.log(rank.join(' '));
