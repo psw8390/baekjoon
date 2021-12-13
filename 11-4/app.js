5c8becb33db41332ba3ff443da834cae0c8e307b
{psw9058) <s05150732w@gmail.com>
2021-12-13 11:35:19 +0900

    11-4 submit2

--- 11-4/app.js
+++ 11-4/app.js
@@ -1,12 +1,42 @@
 const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const input = fs.readFileSync('./input.txt').toString().trim().split('\n');
+ 
+  const NM = input
+    .shift()
+    .split(' ')
+    .map(num => parseInt(num));
+  const N = NM.shift();
+  const M = NM.shift();
+  const minArr = [];
+   
+  const whiteFirst = [
+  'WBWBWBWB',
+  'BWBWBWBW',
+  'WBWBWBWB',
+  'BWBWBWBW',
+  'WBWBWBWB',
+  'BWBWBWBW',
+  'WBWBWBWB',
+  'BWBWBWBW'
+  ];
+   
+  const blackFirst = [
+  'BWBWBWBW',
+  'WBWBWBWB',
+  'BWBWBWBW',
+  'WBWBWBWB',
+  'BWBWBWBW',
+  'WBWBWBWB',
+  'BWBWBWBW',
+  'WBWBWBWB'
+  ];
 
 function whiteFirstPaint(y, x) {
-  let counter = 0;
-  for (let i = y; i < y + 8; i++)
-  for (let j = x; j < x + 8; j++)
-  if (input[i][j] !== whiteFirst[i - y][j - x]) counter++;
-  return counter;
+    let counter = 0;
+    for (let i = y; i < y + 8; i++)
+    for (let j = x; j < x + 8; j++)
+    if (input[i][j] !== whiteFirst[i - y][j - x]) counter++;
+      return counter;
 }
 
 function blackFirstPaint(y, x) {
@@ -14,14 +44,14 @@ function blackFirstPaint(y, x) {
   for (let i = y; i < y + 8; i++)
   for (let j = x; j < x + 8; j++)
   if (input[i][j] !== blackFirst[i - y][j - x]) counter++;
-  return counter;
-}
-   
-for (let i = 0; i + 7 < N; i++) {
-  for (let j = 0; j + 7 < M; j++) {
-    minArr.push(whiteFirstPaint(i, j));
-    minArr.push(blackFirstPaint(i, j));
+    return counter;
   }
-}
+
+  for (let i = 0; i + 7 < N; i++) {
+    for (let j = 0; j + 7 < M; j++) {
+      minArr.push(whiteFirstPaint(i, j));
+      minArr.push(blackFirstPaint(i, j));
+    }
+  }
 
 console.log(Math.min.apply(null, minArr));
