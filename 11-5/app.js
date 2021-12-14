4b702f8499367e9ee0fceefe0f2a24b0e3312686
{psw9058) <s05150732w@gmail.com>
2021-12-14 12:35:18 +0900

    11-5 submit

--- 11-5/app.js
+++ 11-5/app.js
@@ -1,87 +1,16 @@
-// const fs = require('fs');
-// const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
- 
-const input = [
-'8 8',
-'WBWBWBWB',
-'BWBWBWBW',
-'WBWBWBWB',
-'BWBBBWBW',
-'WBWBWBWB',
-'BWBWBWBW',
-'WBWBWBWB',
-'BWBWBWBW'
-];
- 
-const input = [
-  '10 13',
-  'BBBBBBBBWBWBW',
-  'BBBBBBBBBWBWB',
-  'BBBBBBBBWBWBW',
-  'BBBBBBBBBWBWB',
-  'BBBBBBBBWBWBW',
-  'BBBBBBBBBWBWB',
-  'BBBBBBBBWBWBW',
-  'BBBBBBBBBWBWB',
-  'WWWWWWWWWWBWB',
-  'WWWWWWWWWWBWB'
-  ];
-   
-  const NM = input
-  .shift()
-  .split(' ')
-  .map(num => parseInt(num));
-  const N = NM.shift();
-  const M = NM.shift();
-  const minArr = [];
-   
-  const whiteFirst = [
-  'WBWBWBWB',
-  'BWBWBWBW',
-  'WBWBWBWB',
-  'BWBWBWBW',
-  'WBWBWBWB',
-  'BWBWBWBW',
-  'WBWBWBWB',
-  'BWBWBWBW'
-  ];
-   
-  const blackFirst = [
-  'BWBWBWBW',
-  'WBWBWBWB',
-  'BWBWBWBW',
-  'WBWBWBWB',
-  'BWBWBWBW',
-  'WBWBWBWB',
-  'BWBWBWBW',
-  'WBWBWBWB'
-  ];
-   
-  function whiteFirstPaint(y, x) {
-  let counter = 0;
-   
-  for (let i = y; i < y + 8; i++)
-  for (let j = x; j < x + 8; j++)
-  if (input[i][j] !== whiteFirst[i - y][j - x]) counter++;
-   
-  return counter;
+const fs = require('fs');
+const input = parseInt(fs.readFileSync('/dev/stdin').toString().trim());
+
+
+let input = 2;
+let theNumberAtTheEnd = 665;
+
+while (input > 0) {
+  theNumberAtTheEnd++;
+
+  if (theNumberAtTheEnd.toString().includes('666')) {
+    input--;
   }
-   
-  function blackFirstPaint(y, x) {
-  let counter = 0;
-   
-  for (let i = y; i < y + 8; i++)
-  for (let j = x; j < x + 8; j++)
-  if (input[i][j] !== blackFirst[i - y][j - x]) counter++;
-   
-  return counter;
-  }
-   
-  for (let i = 0; i + 7 < N; i++) {
-  for (let j = 0; j + 7 < M; j++) {
-  minArr.push(whiteFirstPaint(i, j));
-  minArr.push(blackFirstPaint(i, j));
-  }
-  }
-   
-  console.log(Math.min.apply(null, minArr));
+}
+
+console.log(theNumberAtTheEnd);
