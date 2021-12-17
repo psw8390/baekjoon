1a9762b03bc82b767a52eeae9caf010f5962f061
{psw9058) <s05150732w@gmail.com>
2021-12-17 12:49:51 +0900

    12-1 submit2

--- 12-1/app.js
+++ 12-1/app.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const input = fs.readFileSync('./input.txt').toString().trim().split('\n');
 
 let N = input.shift();
-let result = input.sort();
+let result = input.sort((a,b) => a-b);
 
 for(let i = 0; i < N; i++) {
   console.log(result[i]);
