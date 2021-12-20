a22f9b68a7cdfa61c56c91b9f76c63cb1880211e
{psw9058) <s05150732w@gmail.com>
2021-12-20 12:26:36 +0900

    12-2 submit

--- 12-2/app.js
+++ 12-2/app.js
@@ -1,37 +1,9 @@
-const input = [10, 5, 2, 3, 1, 4, 2, 3, 5, 1, 7];
+const fs = require('fs');
+const input = fs.readFileSync('./input.txt').toString().trim().split('\n');
+
 const N = input.shift();
 
-const result = quickSortStarter(input).join('\n');
+let results = '';
+input.sort((a, b) => a - b).forEach(num => (results += `${num}\n`));
 
-function quickSortStarter(arr) {
-  if (!arr.length) {
-    return arr;
-  }
-  return quickSort(arr, 0, arr.length - 1);
-}
-
-function quickSort(array, l, r) {
-  const pivot = array[Math.floor((l + r) / 2)];
-  let left = l;
-  let right = r;
-
-  while (left <= right) {
-    while (array[left] < pivot) left++;
-    while (array[right] > pivot) right--;
-
-    if (left <= right) {
-      const temp = array[left];
-      array[left] = array[right];
-      array[right] = temp;
-      left++;
-      right--;
-    }
-  }
-
-  if (l < right) quickSort(array, l, right);
-  if (r > left) quickSort(array, left, r);
-
-  return array;
-}
-
-console.log(result);
+console.log(results);
