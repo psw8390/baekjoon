424bd475ff58e88fb1c2bbdc2de40cb44512a470
{psw9058) <s05150732w@gmail.com>
2021-12-21 11:46:21 +0900

    12-2 submit2

--- 12-2/app.js
+++ 12-2/app.js
@@ -1,9 +1,39 @@
 const fs = require('fs');
-const input = fs.readFileSync('./input.txt').toString().trim().split('\n');
+const input = fs.readFileSync('./input.txt').toString().trim().split('\n').map(num => parseInt(num, 10));
 
 const N = input.shift();
 
-let results = '';
-input.sort((a, b) => a - b).forEach(num => (results += `${num}\n`));
+const result = quickSortStarter(input).join('\n');
 
-console.log(results);
+function quickSortStarter(arr) {
+  if (!arr.length) {
+    return arr;
+  }
+  return quickSort(arr, 0, arr.length - 1);
+}
+
+function quickSort(array, l, r) {
+  const pivot = array[Math.floor((l + r) / 2)];
+  let left = l;
+  let right = r;
+
+  while (left <= right) {
+    while (array[left] < pivot) left++;
+    while (array[right] > pivot) right--;
+
+    if (left <= right) {
+      const temp = array[left];
+      array[left] = array[right];
+      array[right] = temp;
+      left++;
+      right--;
+    }
+  }
+
+  if (l < right) quickSort(array, l, right);
+  if (r > left) quickSort(array, left, r);
+
+  return array;
+}
+
+console.log(result);
