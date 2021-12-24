8f9537d0ca81789a92fab3eceb8bf48f0349c665
{psw9058) <s05150732w@gmail.com>
2021-12-24 16:50:46 +0900

    12-3

--- 12-3/app.js
+++ 12-3/app.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 const input = fs.readFileSync('./input.txt').toString().trim().split('\n').map(num => parseInt(num, 10));
 
 const max = Math.max(...input);
-const N = input.shift();
 
 const array = new Array(max + 1);
 array.fill(0);
 
 for (let i = 0; i < input.length; i++) {
   array[input[i]]++;
+  console.log(array);
 }
 
 for (let i = 0; i < array.length; i++) {
